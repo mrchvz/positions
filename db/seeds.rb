08abf84d14c88f65b5e0047172ffd89adebb7657
mrchvz <chavezmmarie@gmail.com>
2020-09-18 10:21:30 -0700

    second commit

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,22 +10,19 @@ $position_names = {
   }
 }
 
-class Testing
-  def create_positions(parent_position, tree)
-    tree.each do |name, value|
-      parent_position = Position.create!(name: name, parent_position: parent_position)
-      case value
-      when Hash
-        create_positions(parent_position, value)
-      when Symbol
-        Position.create!(name: value, parent_position: parent_position)
-      else
-        raise StandardError
-      end
-    end
-  end
+def create_positions(parent_position, tree) # se define el metodo con parent_position y tree como argumentos
+  tree.each do |name, value| # iteramos sobre el hash
+    parent_position = Position.create!(name: name, parent_position: parent_position) # declaramos la variable parent_position en la que crearemos nuevas instancias
+    case value # case statement
+    when Hash
+      create_positions(parent_position, value) # cuando el valor es un hash se crea parent_position con el value del hash
+    when Symbol
+      Position.create!(name: value, parent_position: parent_position) # cuando el valor es un symbol se crea el nombre con el valor correspondiente, y parent_position es parent_position
+    else
+      raise StandardError # cuando el valor no es un hash o symbol se levanta una excepción
+    end # se cierra es case statement, linea 19
+  end # se cierra la iteracion, linea 17
+end #se cierra el metodo, linea 16
 
-  create_positions(nil, $position_names)
-
-end
 # Go!
+create_positions(nil, $position_names) # usamos el metodo dentro del metodo
